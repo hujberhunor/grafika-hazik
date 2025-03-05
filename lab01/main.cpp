1908951be2684962fa709b0d5501e12ccfd18e6a
Hujber Hunor <hujber.hunor@gmail.com>
2025-03-05 18:38:39 +0100

    Egyenesek működnek, köszi Csabi

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,6 +1,9 @@
 #include "./framework.h"
+#include <cmath>
+#include <cstdio>
 #include <glm/detail/qualifier.hpp>
 #include <glm/ext/vector_float2.hpp>
+#include <iostream>
 
 // cs�cspont �rnyal�
 const char *vertSource = R"(
@@ -28,36 +31,84 @@ const char *fragSource = R"(
 )";
 
 const int winWidth = 600, winHeight = 600;
+char keyState = ' ';
 
 class VertexApp : public glApp{
-    Geometry<vec2> *vertex1; // VERTECIES
+    Geometry<vec2> *vertices; // Pontok tárolása
+    Geometry<vec2> *line;
     GPUProgram* gpuProgram;
 
 public:
     VertexApp() : glApp("VertexApp-lab01") {}
 
     void onInitialization(){
-        vertex1 = new Geometry<vec2>;
-
-        vertex1->Vtx() = {vec2(0.0f, 0.0f)};
-
-        vertex1 -> updateGPU(); // GPU-ra feltöltés
-
+        vertices = new Geometry<vec2>;
+        line = new Geometry<vec2>;
         gpuProgram = new GPUProgram(vertSource, fragSource);
+    }
+
+    void onKeyboard(int key) override{
+        keyState = key;
+    }
+
+    vec2 closestPoint(float pX, float pY) {
+        // NORMALIZÁLÁS
+        // float ndcX = 2.0f * (pX / (float) winWidth) - 1.0f;
+        // float ndcY = 1.0f - 2.0f * (pY / (float)winHeight);
+
+        // float minDist = std::numeric_limits<float>::max(); // Kezdetben végtelen nagy távolság
+        float minDist = 2.0; // mert 1.0 a max
+        vec2 closestPoint;
+
+        // Végigmegyünk az összes ponton, és megkeressük a legközelebbit
+        for (const vec2& point : vertices->Vtx()) {
+            float dist = sqrt(pow(point.x - pX, 2) + pow(point.y - pY, 2));
+            if (dist < minDist) {
+                minDist = dist;
+                closestPoint = point;
+            }
+        }
+        printf("Legközelebbi pont: (%.2f, %.2f), Távolság: %.4f\n", closestPoint.x, closestPoint.y, minDist);
+        return closestPoint;
     }
 
     void onMousePressed(MouseButton but, int pX, int pY) {
         if (but == MOUSE_LEFT) {
-
-            // Windows koordináta NDC-be konvertálása
             float ndcX = 2.0f * (pX / (float) winWidth) - 1.0f;
             float ndcY = 1.0f - 2.0f * (pY / (float)winHeight);
 
-            printf("Egérkattintás: (%.2f, %.2f) \n", ndcX, ndcY);
+            if (keyState == 'p') {
+                printf("Egérkattintás: (%.2f, %.2f) \n", ndcX, ndcY);
+                vertices->Vtx().push_back(vec2(ndcX, ndcY));
+                vertices->updateGPU();
+            }
+            else if (keyState == 'l') {
+                printf("Egyenes: ");
+                vec2 p1 = closestPoint(ndcX, ndcY);
+                line->Vtx().push_back(p1);
 
-            vertex1->Vtx().push_back(vec2(ndcX, ndcY));
-            vertex1->updateGPU(); // GPU frissítés
-            refreshScreen(); // Azonnali újrarajzolás
+                vec2 pl1 = line->Vtx()[line->Vtx().size()-1];
+                vec2 pl2 = line->Vtx()[line->Vtx().size()-2];
+                vec2 v = pl2-pl1; // irányvektor
+
+                if(line->Vtx().size() % 2 == 0){
+                    v = vec2(v.x * 10000, v.y * 10000);
+                    pl1 = pl1 + v;
+                    pl2 = pl2 - v ;
+                }
+
+                line->Vtx()[line->Vtx().size()-1] = pl1;
+                line->Vtx()[line->Vtx().size()-2] = pl2;
+                line->updateGPU();
+            }
+            else if (keyState == 'm') {
+                // Mozgatás
+            }
+            else if (keyState == 'i') {
+                // Metszéspont számítás
+            }
+
+            refreshScreen();
         }
     }
 
@@ -66,8 +117,10 @@ public:
         glClear(GL_COLOR_BUFFER_BIT);
         glViewport(0,0, winWidth, winHeight);
         glPointSize(10.0f);
+        glLineWidth(3.0f);
 
-        vertex1 -> Draw(gpuProgram, GL_POINTS, vec3(1.0f, 0.0f, 0.0f));
+        vertices -> Draw(gpuProgram, GL_POINTS, vec3(1.0f, 0.0f, 0.0f));
+        line -> Draw(gpuProgram, GL_LINES, vec3(0.0f, 1.0f, 1.0f));
     }
 };
 
