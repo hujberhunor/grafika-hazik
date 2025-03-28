4a7c1d207550d1b1e843a5b4f9fc54c265fb29c9
Hujber Hunor <hujber.hunor@gmail.com>
2025-03-28 21:10:22 +0100

    Gondola előtti camera és deriváltak

--- lab02/main.cpp
+++ lab02/main.cpp
@@ -32,9 +32,9 @@ const int winWidth = 600, winHeight = 600;
 GPUProgram *gpuProgram;
 
 /*
-* --- Kamera osztály ---
-* Másolva a ppt-ből
-*/
+ * --- Kamera osztály ---
+ * Másolva a ppt-ből
+ */
 
 class Camera2D {
   vec2 wCenter = vec2(0, 0); // center in world coords
@@ -59,83 +59,114 @@ public:
     vec4 worldCoords = Pinv() * Vinv() * ndcCoords;
     return vec3(worldCoords.x, worldCoords.y, 1.0f);
   }
+
+  mat4 MVP() { return P() * V(); }
 };
 
 /*
-* CatmullRom spline
-*/
+ * CatmullRom spline
+ */
 class Spline {
-    Geometry<vec3> cps; // control points
-    Geometry<float> ts; // parameter (knot) values
-    // Folytonos, sima görbét hozhatunk létre a pontok és azokhoz tartozó érintővektorok (sebességek) alapján.
-    vec3 Hermite(vec3 p0, vec3 v0, float t0, vec3 p1, vec3 v1, float t1, float t) {
-        float dt = t1 - t0;
-        float u = (t - t0) / dt;
-        float u2 = u * u;
-        float u3 = u2 * u;
+  Geometry<vec3> cps; // control points
+  Geometry<float> ts; // parameter (knot) values
+  // Folytonos, sima görbét hozhatunk létre a pontok és azokhoz tartozó
+  // érintővektorok (sebességek) alapján.
+  vec3 Hermite(vec3 p0, vec3 v0, float t0, vec3 p1, vec3 v1, float t1,
+               float t) {
+    float dt = t1 - t0;
+    float u = (t - t0) / dt;
+    float u2 = u * u;
+    float u3 = u2 * u;
 
-        vec3 a0 = p0;
-        vec3 a1 = v0;
-        vec3 a2 = (3.0f * (p1 - p0) / (dt * dt)) - ((v1 + 2.0f * v0) / dt);
-        vec3 a3 = (2.0f * (p0 - p1) / (dt * dt * dt)) + ((v1 + v0) / (dt * dt));
+    vec3 a0 = p0;
+    vec3 a1 = v0;
+    vec3 a2 = (3.0f * (p1 - p0) / (dt * dt)) - ((v1 + 2.0f * v0) / dt);
+    vec3 a3 = (2.0f * (p0 - p1) / (dt * dt * dt)) + ((v1 + v0) / (dt * dt));
 
-        return a3 * u3 + a2 * u2 + a1 * u + a0;
+    return a3 * u3 + a2 * u2 + a1 * u + a0;
+  }
+
+  std::vector<vec3> &cp = cps.Vtx(); // alias a vezérlőpontokra
+  std::vector<float> &t = ts.Vtx();  // alias a csomóértékekre
+
+public:
+  void AddControlPoint(vec3 p) {
+    float ti = cp.size();
+    cp.push_back(p);
+    t.push_back(ti);
+
+    cps.updateGPU();
+  }
+
+  // Lokálja meglyik szakaszra esik a t paraméter
+  vec3 r(float param) {
+    for (int i = 0; i < cp.size() - 1; i++) {
+      if (t[i] <= param && param <= t[i + 1]) {
+        vec3 v0(0, 0, 0), v1(0, 0, 0);
+        if (i > 0)
+          v0 = (cp[i] - cp[i - 1]) / (t[i] - t[i - 1]);
+        if (i < cp.size() - 2)
+          v1 = (cp[i + 1] - cp[i]) / (t[i + 1] - t[i]);
+        return Hermite(cp[i], v0, t[i], cp[i + 1], v1, t[i + 1], param);
+      }
+    }
+    // TOOD KEZELNI
+    return vec3(0, 0, 0);
+  }
+
+  vec3 HermiteDeriv(vec3 p0, vec3 v0, float t0, vec3 p1, vec3 v1, float t1,
+                    float t) {
+    float dt = t1 - t0;
+    float u = (t - t0) / dt;
+    float u2 = u * u;
+
+    vec3 a2 = (3.0f * (p1 - p0) / (dt * dt)) - ((v1 + 2.0f * v0) / dt);
+    vec3 a3 = (2.0f * (p0 - p1) / (dt * dt * dt)) + ((v1 + v0) / (dt * dt));
+
+    return (3.0f * a3 * u2 + 2.0f * a2 * u + v0);
+  }
+
+  // derivált r
+  vec3 dr(float param) {
+    for (int i = 0; i < cp.size() - 1; i++) {
+      if (t[i] <= param && param <= t[i + 1]) {
+        vec3 v0(0, 0, 0), v1(0, 0, 0);
+        if (i > 0)
+          v0 = (cp[i] - cp[i - 1]) / (t[i] - t[i - 1]);
+        if (i < cp.size() - 2)
+          v1 = (cp[i + 1] - cp[i]) / (t[i + 1] - t[i]);
+        return HermiteDeriv(cp[i], v0, t[i], cp[i + 1], v1, t[i + 1], param);
+      }
+    }
+    return vec3(0, 0, 0);
+  }
+
+  void drawSpline(GPUProgram *gpu) {
+    std::vector<vec3> points;
+    if (cp.size() < 2)
+      return;
+
+    // Görbe pontjainak kiszámítása
+    float dt = 0.01f; // lépésköz
+    for (float param = t[0]; param <= t[t.size() - 1]; param += dt) {
+      points.push_back(r(param));
     }
 
-    std::vector<vec3>& cp = cps.Vtx(); // alias a vezérlőpontokra
-    std::vector<float>& t = ts.Vtx();  // alias a csomóértékekre
+    Geometry<vec3> curve;
+    curve.Vtx() = points;
+    curve.updateGPU();
+    curve.Draw(gpu, GL_LINE_STRIP, vec3(1.0f, 1.0f, 0.0f)); // sárga pontok
 
-    public:
-
-    void AddControlPoint(vec3 p) {
-        float ti = cp.size();
-        cp.push_back(p);
-        t.push_back(ti);
-
-        cps.updateGPU();
-    }
-
-    // Lokálja meglyik szakaszra esik a t paraméter
-    vec3 r(float param) {
-        for (int i = 0; i < cp.size() - 1; i++) {
-            if (t[i] <= param && param <= t[i + 1]) {
-                vec3 v0(0, 0, 0), v1(0, 0, 0);
-                if (i > 0) v0 = (cp[i] - cp[i - 1]) / (t[i] - t[i - 1]);
-                if (i < cp.size() - 2) v1 = (cp[i + 1] - cp[i]) / (t[i + 1] - t[i]);
-                return Hermite(cp[i], v0, t[i], cp[i + 1], v1, t[i + 1], param);
-            }
-        }
-        // TOOD KEZELNI
-        return vec3(0, 0, 0);
-    }
-
-
-    void drawSpline(GPUProgram* gpu) {
-        std::vector<vec3> points;
-        if (cp.size() < 2) return;
-
-        // Görbe pontjainak kiszámítása
-        float dt = 0.01f; // lépésköz
-        for (float param = t[0]; param <= t[t.size() - 1]; param += dt) {
-            points.push_back(r(param));
-        }
-
-        Geometry<vec3> curve;
-        curve.Vtx() = points;
-        curve.updateGPU();
-        curve.Draw(gpu, GL_LINE_STRIP, vec3(1.0f, 1.0f, 0.0f)); // sárga pontok
-
-        cps.updateGPU();
-        cps.Draw(gpu, GL_POINTS, vec3(1.0f, 0.0f, 0.0f));   // piros vonal
-    }
+    cps.updateGPU();
+    cps.Draw(gpu, GL_POINTS, vec3(1.0f, 0.0f, 0.0f)); // piros vonal
+  }
 }; // END OF SPLINE
-
 
 // Kocsi class
 class Gondola;
 
 class Main : public glApp {
-  Spline* spline;
+  Spline *spline;
   GPUProgram *gpuProgram;
 
 public:
@@ -147,25 +178,25 @@ public:
   }
 
   // Pontok hozzáadása
-  void onMousePressed(MouseButton but, int pX, int pY) override{
-      if (but == MOUSE_LEFT) {
-        float ndcX = 2.0f * (pX / (float) winWidth) - 1.0f;
-        float ndcY = 1.0f - 2.0f * (pY / (float)winHeight);
+  void onMousePressed(MouseButton but, int pX, int pY) override {
+    if (but == MOUSE_LEFT) {
+      float ndcX = 2.0f * (pX / (float)winWidth) - 1.0f;
+      float ndcY = 1.0f - 2.0f * (pY / (float)winHeight);
 
-        spline->AddControlPoint(vec3(ndcX, ndcY, 0));
-        refreshScreen();
-      }
+      spline->AddControlPoint(vec3(ndcX, ndcY, 0));
+      refreshScreen();
+    }
   }
 
   void onDisplay() {
-      glClearColor(0.0f, 0.0f, 0.0f, 1);
-      glClear(GL_COLOR_BUFFER_BIT);
-      glViewport(0,0, winWidth, winHeight);
-      glPointSize(10.0f);
-      glLineWidth(3.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, winWidth, winHeight);
+    glPointSize(10.0f);
+    glLineWidth(3.0f);
 
-      // Controll pontok és görbék kirajzolása
-      spline->drawSpline(gpuProgram);
+    // Controll pontok és görbék kirajzolása
+    spline->drawSpline(gpuProgram);
   }
 };
 
